34654658fbc525a814102888c2c4be9904339128
aricept <aricept@users.noreply.github.com>
2019-12-31 15:13:23 -0600

    Fix typo

--- WebGridExtensions/WebGridExtensions.cs
+++ WebGridExtensions/WebGridExtensions.cs
@@ -17,7 +17,7 @@ namespace WebGridExtensions
         /// <param name="html"></param>
         /// <param name="exp">Lambda expression for column property.</param>
         /// <param name="format">Function used to format the data item that is associated with the WebGrid column.</param>
-        /// <param name="style">The CSS class attribute that is rendered as part of the HTML table cless that are associated with the WebGrid column.</param>
+        /// <param name="style">The CSS class attribute that is rendered as part of the HTML table cells that are associated with the WebGrid column.</param>
         /// <param name="canSort">Indicates whether the column can be sorted.</param>
         /// <returns>Data column</returns>
         public static WebGridColumn GridColumnFor<TModel, TValue>(this HtmlHelper<IEnumerable<TModel>> html, Expression<Func<TModel, TValue>> exp, Func<dynamic, object> format = null, string style = null, bool canSort = true)
